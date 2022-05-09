8a7f5011aac14070151bc297bf5aa92763ee134e
Filip Łasica <white@MacBook-Pro-Filip.local>
2022-05-09 15:23:35 +0200

    Add structure

---
+++ src/components/page.svelte
@@ -0,0 +1,7 @@
+<script> 
+    import PostCard from './post-card.svelte' //import the postcard component
+</script>
+  
+<PostCard title="Hello">
+    <p>Hello</p>
+</PostCard>
---
+++ src/components/post-card.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let title; //make variable available
+</script>
+  
+<h1>{title}</h1> //use the variable value
